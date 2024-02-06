8b9c42a5bc6c2e80c30b1e7a9f6355df5b7792ec
Pravesh Niroula <pniroula366@gmail.com>
2024-02-06 22:05:55 +0545

    no mail thingy

--- server/routes/mails.js
+++ server/routes/mails.js
@@ -1,8 +1,8 @@
 const route = require('express').Router();
 
-const { sendTestingMail, sendGmail } = require("../controllers/mailController");
+const { sendTestingMail } = require("../controllers/mailController");
 
 route.post('/send', sendTestingMail);
-route.post('/sendgmail', sendGmail);
+// route.post('/sendgmail', sendGmail);
 
 module.exports = route;
